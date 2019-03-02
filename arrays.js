1cc17bf1aa0da60e62c72a88a080b259122ad2d0
Rene Cocom <fasnimo@gmail.com>
2019-03-02 07:37:09 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -39,6 +39,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(-0)
-  return array(-0)
+  array.slice()
+  return array
 }
