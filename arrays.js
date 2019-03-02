2e8796acc4602c035c1b42d7846690d71e3f4cfe
Rene Cocom <fasnimo@gmail.com>
2019-03-02 07:11:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -20,7 +20,7 @@ function destructivelyAddElementToEndOfArray (array, element) {
   }
 
 function accessElementInArray (array, index) {
- console.log(array[2])
+ return (array[2])
 }
 // function accessElementInArray () {
   
